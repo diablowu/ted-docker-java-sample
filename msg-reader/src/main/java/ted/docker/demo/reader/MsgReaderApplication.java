2e9302103466470b00b29895330a9299e317dbc2
Diablo Wu <wubo19842008@gmail.com>
2018-05-17 15:56:42 +0800

    init

---
+++ msg-reader/src/main/java/ted/docker/demo/reader/MsgReaderApplication.java
@@ -0,0 +1,40 @@
+package ted.docker.demo.reader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class MsgReaderApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgReaderApplication.class, args);
+	}
+}
+
+
+@RestController
+class ReaderControll {
+
+	@Autowired
+	private StringRedisTemplate redis;
+
+	@GetMapping("/{ch}/msg")
+	public ResponseEntity<String> get(@PathVariable(name="ch") String ch){
+		String r = redis.opsForList().leftPop(ch);
+		if(StringUtils.hasText(r)){
+			return ResponseEntity.ok(r);
+		}else{
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
---
+++ msg-writer/src/main/java/ted/docker/demo/writer/MsgWriterApplication.java
@@ -0,0 +1,35 @@
+package ted.docker.demo.writer;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MsgWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgWriterApplication.class, args);
+	}
+}
+
+@RestController
+class WriterControll {
+
+	@Autowired
+	private StringRedisTemplate redis;
+
+	@RequestMapping("/{ch}/msg")
+	public ResponseEntity<String> get(@PathVariable(name = "ch") String ch,String msg) {
+		long len = redis.opsForList().rightPush(ch,msg + "@" + LocalDateTime.now().toString());
+		return ResponseEntity.ok(String.valueOf(len));
+
+	}
+
+}
